7392efb8cb71ef94a12968f936fe31aafeb2ba03
evitell <vitell@kth.se>
2025-06-18 15:04:24 +0200

    format shells

---
+++ shell1.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    (python3.withPackages (pyPkgs: [ pyPkgs.requests pyPkgs.pandas ]))
+    nodejs_22
+
+  ];
+}
